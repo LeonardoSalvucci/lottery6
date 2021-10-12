574e4c4cec428cd1414a0567f23a041addfffa11
Leonardo Salvucci <le.salvucci@gmail.com>
2021-10-12 12:31:23 -0300

    first commit

---
+++ contracts/TestMap.sol
@@ -0,0 +1,126 @@
+pragma solidity 0.8.7;
+
+import "@chainlink/contracts/src/v0.8/VRFConsumerBase.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract TestMap is VRFConsumerBase, Ownable{
+    // Custom 
+    mapping (uint8 => 
+        mapping (uint8 => 
+            mapping (uint8 => 
+                mapping (uint8 => 
+                    mapping (uint8 => 
+                        mapping (uint8 => 
+                            address[]
+                        )
+                    )
+                )
+            )
+        )
+    ) players;
+    address immutable ownerWallet = 0xD132d3bdA60ed9Edc4ca9F998D8b9B89ccd2AfC1;
+    uint8[6] internal lastResult;
+    
+    event NewLotery(uint8[6] lastResult, bool vacancy, address[] winners);
+    
+    // Random
+    address constant LinkAddress = 0x84b9B910527Ad5C03A9Ca831909E21e236EA7b06;
+    bytes32 internal keyHash;
+    uint256 internal fee;
+    
+    constructor() 
+        VRFConsumerBase(
+            0xa555fC018435bef5A13C6c6870a9d4C11DEC329C, // VRF Coordinator BSC TESTNET
+            LinkAddress  // LINK Token
+        )
+    {
+        keyHash = 0xcaf3c3727e033261d383b315559476f48034c13b18f8cafed4d871abe5049186;
+        fee = 0.1 * 10 ** 18; // 0.1 LINK (Varies by network)
+    }
+    
+    /** 
+     * Requests randomness 
+     */
+    function getRandomNumber() internal returns (bytes32 requestId) {
+        require(LINK.balanceOf(address(this)) >= fee, "Not enough LINK - fill contract with faucet");
+        return requestRandomness(keyHash, fee);
+    }
+
+    /**
+     * Callback function used by VRF Coordinator
+     */
+    function fulfillRandomness(bytes32 requestId, uint256 randomness) internal override {
+        uint8[6] memory randomNumbers;
+        for(uint256 i = 0; i < 6; i++) {
+            randomNumbers[i] = getResultFromRandomness(randomness, i);
+        }
+        lastResult = sortNumbers(randomNumbers);
+        // Here we must verify if someone has win
+        address[] memory winners = verifyIfWinners(
+            lastResult[0],
+            lastResult[1],
+            lastResult[2],
+            lastResult[3],
+            lastResult[4],
+            lastResult[5]
+        );
+        emit NewLotery(lastResult, winners.length>0, winners);
+    }
+    
+    
+    function withdrawLink() external onlyOwner {
+        uint256 totalLinkBalance = IERC20(LinkAddress).balanceOf(address(this));
+        IERC20(LinkAddress).transfer(ownerWallet, totalLinkBalance);
+    }
+    //-----------------------------------------------------
+    
+    
+    function verifyIfWinners(
+        uint8 first, 
+        uint8 second, 
+        uint8 third,
+        uint8 forth,
+        uint8 fifth,
+        uint8 sixth
+    ) internal view returns (address [] memory) {
+        return players[first][second][third][forth][fifth][sixth];
+    }
+    
+    function buyTicket(uint8[6] memory numbers) public returns(bool) {
+        // TODO: prevent that an address cannot be duplicated
+        numbers = sortNumbers(numbers);
+        players[numbers[0]][numbers[1]][numbers[2]][numbers[3]][numbers[4]][numbers[5]].push(msg.sender);
+        return true;
+    }
+    
+    function getLastResult() public view returns(uint8[6] memory) {
+        return lastResult;
+    }
+    
+    function sortNumbers(uint8[6] memory numbers) internal pure returns(uint8[6] memory) {
+        for (uint i = 0; i < 6; i++) {
+            for (uint j = i+1; j < 6; j++) {
+                if(numbers[i] > numbers[j]) {
+                    uint8 temp = numbers[i];
+                    numbers[i] = numbers[j];
+                    numbers[j] = temp;
+                }
+            }
+        }
+        return numbers;
+    }
+    
+    function getResultFromRandomness(uint256 randomness, uint256 i) internal pure returns (uint8){
+        uint8 temp = uint8(uint256(keccak256(abi.encode(randomness, i))));
+        if (temp > 45) {
+            temp = temp % 45;
+        }
+        return temp;
+        
+    }
+    
+    function runLottery() public onlyOwner {
+        getRandomNumber();
+    }
+}
---
+++ hardhat.config.js
@@ -0,0 +1,39 @@
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-etherscan");
+const privKey = '137bfdb5952ca421d407e791a050faa877b9c6408e77ef44b20f5e2998884a72';
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: {
+    version: "0.8.7",
+    settings: {
+      optimizer: {
+        enabled: false,
+        runs: 200,
+      },
+    },
+  },
+  networks: {
+    bsc_testnet: {
+      url: `https://data-seed-prebsc-1-s1.binance.org:8545`,
+      accounts: [`0x${privKey}`]
+    },
+  },
+  etherscan: {
+    apiKey: 'QH4GTKRG2QDKBB3FT9PKANNR18PYZY8A4T'
+  }
+};
